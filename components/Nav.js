255137fef1dc96c80e4217c171a1b8504227a8e8
Aforina <43246362+Aforina@users.noreply.github.com>
2022-02-18 14:59:31 +0100

    logo

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,7 @@
 import { FaBars } from 'react-icons/fa'
 import React, { useState } from 'react'
+import Image from 'next/image'
+
 
 const Nav = ({ servicesRef, tariffsRef, contactRef }) => {
   const [showMenu, setShowMenu] = useState(false)
@@ -41,7 +43,9 @@ const Nav = ({ servicesRef, tariffsRef, contactRef }) => {
     >
       {/*PC*/}
       <div className='hidden h-40 w-full max-w-7xl mx-auto text-white lg:flex flex-row pt-8 p-4 tracking-wider'>
-        <p className='text-3xl cursor-default'>Logo</p>
+        <p className='text-white text-xl'>POSICIONAMIENTO</p>
+        <p className='text-[#FAAC78] text-xl'>WEB</p>
+        <p className='text-white text-xl'>24</p>
         <div className='flex flex-row ml-auto cursor-pointer gap-12 text-2xl'>
           <div className='hover:text-[#FAAC78]' onClick={() => {
             servicesRef.current.scrollIntoView({ behavior: 'smooth' })
@@ -63,7 +67,7 @@ const Nav = ({ servicesRef, tariffsRef, contactRef }) => {
       {/*Mobile*/}
       <div className='lg:hidden flex flex-row absolute text-xl sm:text-4xl sm:gap-80'>
         <div className='flex-1 flex-row w-96 max-w-4xl mx-auto sm:text-3xl text-white pt-6 p-8 tracking-wider'>
-          <p>Logo</p>
+          <Image src='/logo.png' layout='fill' objectFit='cover' />
         </div>
         <div className='flex-1 flex-row pt-8 p-8 w-44 sm:w-64 sm:text-3xl text-white'>
           <FaBars 
